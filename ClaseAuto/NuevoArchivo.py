1982952ee56152d637facb4be56e6ce63d93e0af
MatiasRoel241201 <matias.roel@davinci.edu.ar>
2024-05-07 21:51:44 -0300

    a

---
+++ ClaseAuto/NuevoArchivo.py
@@ -0,0 +1,7 @@
+class nuevaClase:
+    ruedas=4
+    def __init__(self, color, marca, aceleracion, velocidad):
+        self.color= color
+        self.marca= marca
+        self.aceleracion= aceleracion
+        self.velocidad= velocidad
